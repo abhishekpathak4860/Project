64931e4609312e95e29bacc2dd2e737ba650e40b
Abhishek Pathak <abhishekpathak37733@gmail.com>
2025-04-30 20:07:37 +0530

    commit

--- src/components/Lineage.jsx
+++ src/components/Lineage.jsx
@@ -3,7 +3,7 @@ import LineageTreeStructure from "./LineageTreeStructure";
 
 const LineageTree = () => {
   return (
-    <div className=" p-4 sm:p-2 min-h-screen bg-gray-100 border border-red-500 w-full">
+    <div className=" p-4 sm:p-2 min-h-screen bg-[rgb(255,255,240)]  w-full">
       <LineageTreeStructure />
     </div>
   );
--- src/components/Node.jsx
+++ src/components/Node.jsx
@@ -30,7 +30,7 @@ const Node = ({ label, name = "Name", children = [], details, image }) => {
               View Details
             </div>
           </div>
-          <div className="w-0.5 h-8 bg-gray-400 mb-2 z-0 absolute top-65" />
+          <div className="w-0.5 h-8 bg-gray-400 mb-2 z-0 absolute top-45 md:top-65" />
         </>
       ) : (
         <div
